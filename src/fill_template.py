882e822e51e7799618bff23ba39f5e7a651a7e68
DavidA380 <david.greenlee@me.com>
2023-06-23 22:45:09 +0100

    external_file: Fill in a multi sign template using cvs data source

--- src/fill_template.py
+++ src/fill_template.py
@@ -1,64 +1,58 @@
 import pandas as pd
 
 # source template
-template_path = "../test-data/template.svg"
+# template_path = "../test-data/template.svg"
+template_path = "../test-data/batch_example.svg"
 
 # source database csv
 database_path = "../test-data/example.csv"
 
-def get_template_content(template_path):
-    # read template.svg
+def get_template(template_path):
+    # read template.svg which contains the next 12 orchard signs with placeholders
+    # call this when ready to create the next 12 signs
     try:
         with open(template_path, 'r') as template_file:
-            template_content = template_file.read()   # treat this as read only content
+            template = template_file.read()
             template_file.close()
     except FileNotFoundError:
         print(f"File '{template_path}' not found.")
-    return template_content
+    return template
 
-def create_svg_files(template_content, orchard_data_frame):
-    for index, row in orchard_data_frame.iterrows():
-        # replace template placeholders with text from database
-        new_content = template_content.replace("plot", row["Plot Reference"])     
-        new_content = new_content.replace("variety", row["Variety"])    
-        if isinstance(row["Weblink"], str):                                     
-            new_content = new_content.replace("weblink", row["Weblink"])
-        else:
-            new_content = new_content.replace("weblink", " ")
-        if isinstance(row["Rootstock"], str):
-            new_content = new_content.replace("rootstock", row["Rootstock"])
-        else:
-            new_content = new_content.replace("rootstock", " ")
-        if isinstance(row["Pollination Code"], str):
-            new_content = new_content.replace("pollination", row["Pollination Code"])
-        else:
-            new_content = new_content.replace("pollination", " ")
-        if isinstance(row["Notes_1"], str):
-            new_content = new_content.replace("notes_1", row["Notes_1"])
-        else:
-            new_content = new_content.replace("notes_1", " ")
-        if isinstance(row["Notes_2"], str):
-            new_content = new_content.replace("notes_2", row["Notes_2"])
-        else:
-            new_content = new_content.replace("notes_2", " ")
-
-        # save the created new content as an svg file
-        output_path = "../output/plot" + row["Plot Reference"] + ".svg"
-        try:
-            with open(output_path, 'w') as output_file:
-                output_file.write(new_content)
-                output_file.close()
-                # print("Created file for plot", row["Plot Reference"])
-        except FileNotFoundError:
-                print(f"File '{output_path}' not created.")
+def update_template(template_content, row, index):
+    # input:
+    #   the template as perhaps modified by this function
+    #   the database row 
+    #   the sign sequence number 0..last
+    # return:
+    #   the modified template
     
+    index_str = str(index)
+    
+    new_content = template_content.replace("PLOT_"+ index_str, str(row[1]["Plot Reference"]))
+    
+    new_content = new_content.replace("VARIETY_"+ index_str, str(row[1]["Variety"])) 
+        
+    if isinstance(row[1]["Rootstock"], str):
+        new_content = new_content.replace("ROOT_"+ index_str, str(row[1]["Rootstock"]))
+    else:
+        new_content = new_content.replace("ROOT_"+ index_str, " ")
+        
+    if isinstance(row[1]["Pollination Code"], str):
+        new_content = new_content.replace("POLL_"+ index_str, str(row[1]["Pollination Code"]))
+    else:
+        new_content = new_content.replace("POLL_"+ index_str, " ")
+        
+    # return the perhaps partially updated template
+    return new_content
 
 
-# # read the database csv with string encoding forced
-# my_orchard_data_frame = pd.read_csv(database_path, dtype="string")
-
-# # read the template svg
-# my_template_content = get_template_content(template_path)
-
-# # replace template placeholders with data and save as new svg files
-# create_svg_files(my_template_content, my_orchard_data_frame)
+def save_template(template_content, append_filename_str):
+    # save the updated template file
+    output_path = "../output/orchard_" + append_filename_str + ".svg"
+    try:
+        with open(output_path, 'w') as output_file:
+            output_file.write(template_content)
+            output_file.close()
+            print("Created ", output_path)
+    except FileNotFoundError:
+            print(f"File '{output_path}' not created.")
